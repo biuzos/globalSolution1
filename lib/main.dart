b52397d8f6f07bb37f51b799d141b03e577c8687
biuzos <bielhenko@gmail.com>
2023-05-29 19:30:07 -0300

    tirando debug

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:projeto_gs1/screens/homescreen.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Directionality(
         textDirection: TextDirection.ltr,
         child: TelaInicial(),
